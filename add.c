92ae86d9ef0c551a6dd9fde35690480d8a5b6fc3
iam-harshitha <lharshitha21@gmail.com>
2024-02-22 14:58:24 +0530

    changes

--- add.c
+++ add.c
@@ -3,7 +3,7 @@
 int main() {
     int num1, num2, sum ;
     
-    printf("enter the first num: ");
+    printf(" the first number: ");
     scanf("%d", &num1);
     
     printf("Enter the second number: ");
